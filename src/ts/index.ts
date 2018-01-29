ac477c948c777c8376bb2ec3b8cdc2e44b1c7d4c
Camille PIGNON <cpignon@itlink.fr>
2018-01-29 18:18:59 +0100

    Finished the basic working app.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,12 +1,23 @@
 require('./../scss/index.scss');
 
-class Game {
+import 'pixi';
+import 'p2';
+import 'phaser-ce';
+
+export class Game {
+    game: Phaser.Game;
 
     constructor() {
-
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {preload: this.preload, create: this.create});
     }
 
+    preload() {
+        // TODO
+    }
 
+    create() {
+        // TODO
+    }
 };
 
 window.onload = () => {
--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,13 @@ const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const rootDir = path.resolve( __dirname );
+
+const phaserModule = path.join(rootDir, "node_modules", "phaser-ce");
+const phaser = path.join(phaserModule, "build", "custom", "phaser-split.js");
+const pixi = path.join(phaserModule, "build", "custom", "pixi.js");
+const p2 = path.join(phaserModule, "build", "custom", "p2.js");
+
 let config = {
     entry: {
         app: "./src/ts/index.ts"
@@ -20,10 +27,24 @@ let config = {
                 fallback: 'style-loader',
                 use: ['css-loader', 'sass-loader', 'postcss-loader']
             }))
+        }, {
+            test: /pixi\.js/,
+            loader: "expose-loader?PIXI"
+        }, {
+            test: /phaser-split\.js/,
+            loader: "expose-loader?Phaser"
+        }, {
+            test: /p2\.js/,
+            loader: "expose-loader?p2"
         }]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            'pixi': pixi,
+            'p2': p2,
+            'phaser-ce': phaser
+        }
     },
     plugins: [
         new CleanWebpackPlugin(['public']),
